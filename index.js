b1a7f56dc30d4705a8e0e9016d3aebf48afa6d49
firstboen@gmail.com <firstboen@gmail.com>
2018-05-26 09:18:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var a = Math.floor(Math.random() * 1000)
 var b = Math.floor(Math.random() * 1000)
 
-function add() {
-  var c = a + b 
-  return c  
+function add(a,b) {
+   
+  return a+b  
   
 }
